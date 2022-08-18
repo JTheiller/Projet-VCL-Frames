e87506866a91bc41501542f156d3d80bf658cb64
Joathan Theiller <jtheiller@hotmail.com>
2022-08-18 12:17:30 -0300

    Removido custom manifest, alertado se modo admin pós abertura

--- src/view/main.view.pas
+++ src/view/main.view.pas
@@ -13,11 +13,14 @@ type
     Frame21: TFrame2;
     Frame13: TFrame1;
     Frame22: TFrame2;
+    procedure FormCreate(Sender: TObject);
   private
     { Private declarations }
   public
     { Public declarations }
   end;
+
+  procedure isAdmin();
 
 var
   FormMain: TFormMain;
@@ -25,5 +28,36 @@ var
 implementation
 
 {$R *.dfm}
+
+uses
+  System.Win.Registry;
+
+procedure isAdmin();
+var
+  reg: TRegistry;
+  openResult: Boolean;
+begin
+  reg := TRegistry.Create(KEY_READ);
+  reg.RootKey := HKEY_LOCAL_MACHINE;
+  reg.Access := KEY_WRITE;
+  openResult := reg.OpenKey('Software\MyCompanyName\MyApplication\',True);
+  if not openResult = True then
+    begin
+    MessageDlg('Unable to write to registry. Your application does NOT have Administrator level privileges.',
+                TMsgDlgType.mtError, mbOKCancel, 0);
+    end
+  else
+    begin
+    MessageDlg('Write to registry permitted. Your application has Administrator level privileges.',
+                TMsgDlgType.mtInformation, mbOKCancel, 0);
+    end;
+  reg.CloseKey();
+  reg.Free;
+end;
+
+procedure TFormMain.FormCreate(Sender: TObject);
+begin
+  isAdmin();
+end;
 
 end.
